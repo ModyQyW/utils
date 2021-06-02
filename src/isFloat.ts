78c0a14ee32bcbd62890de97467d137cffa20373
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-06-03 06:55:12 +0800

    chore: update jsdoc

--- src/isFloat.ts
+++ src/isFloat.ts
@@ -3,8 +3,8 @@ import { isNumber, isNaN, isFinite, isInteger } from 'lodash-es';
 
 /**
  * Checks if value is classified as a Number primitive or object.
- * @param value The value to check
- * @returns Returns true if value is correctly classified, else false.
+ * @param value - The value to check
+ * @return - Returns true if value is correctly classified, else false.
  */
 export function isFloat(value?: any) {
   return (
