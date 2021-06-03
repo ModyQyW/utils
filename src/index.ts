e7a265f093d1c3dae732eecca860e6a3e0c4b1e9
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-06-03 08:20:40 +0800

    feat: add isSyntaxError

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * from './isEvalError';
 export * from './isFloat';
 export * from './isRangeError';
 export * from './isReferenceError';
+export * from './isSyntaxError';
 export * from './isValidDate';
