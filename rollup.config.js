fb809bb9c914257a4a893305da0b6f83b8b4e9f7
wurui <wurui7@mail3.sysu.edu.cn>
2021-06-09 14:19:56 +0800

    build: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
+import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 import dts from 'rollup-plugin-dts';
@@ -7,26 +9,6 @@ import path from 'path';
 const configs = [];
 
 configs.push(
-  // {
-  //   input: path.resolve('src', 'index.ts'),
-  //   output: [
-  //     {
-  //       file: path.resolve('dist', 'index.cjs.js'),
-  //       format: 'cjs',
-  //     },
-  //     {
-  //       file: path.resolve('dist', 'index.esm.js'),
-  //       format: 'es',
-  //     },
-  //     {
-  //       file: path.resolve('dist', 'index.iife.js'),
-  //       format: 'iife',
-  //       name: 'UTILS',
-  //       extend: true,
-  //     },
-  //   ],
-  //   plugins: [nodeResolve(), typescript()],
-  // },
   {
     input: path.resolve('src', 'index.ts'),
     output: [
@@ -51,7 +33,11 @@ configs.push(
           comments: false,
         },
       }),
+      nodePolyfills(),
       nodeResolve(),
+      commonjs({
+        include: /node_modules/,
+      }),
       typescript(),
     ],
   },
