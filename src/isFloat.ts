fcdc31b5968732bd62ad3f19fd63af658a62ccc0
wurui <wurui7@mail3.sysu.edu.cn>
2021-06-10 15:44:42 +0800

    style: add period

--- src/isFloat.ts
+++ src/isFloat.ts
@@ -3,7 +3,7 @@ import { isNumber, isNaN, isFinite, isInteger } from 'lodash-es';
 
 /**
  * Checks if value is classified as a Number primitive or object.
- * @param value - The value to check
+ * @param value - The value to check.
  * @return - Returns true if value is correctly classified, else false.
  */
 export const isFloat = (value?: any) =>
