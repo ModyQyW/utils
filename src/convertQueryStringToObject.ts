9e8d14284864c9404f30faf562771c98c75a7d5f
wurui <wurui7@mail3.sysu.edu.cn>
2021-06-10 15:39:23 +0800

    feat: parseQueryString -> convertQueryStringToObject

---
+++ src/convertQueryStringToObject.ts
@@ -0,0 +1,13 @@
+/* eslint-disable import/prefer-default-export */
+import QueryString, { parse } from 'qs';
+
+/**
+ * Convert query string to object.
+ * @param string The query string to convert.
+ * @param options Options used to convert.
+ * @returns - Object.
+ */
+export const convertQueryStringToObject = (
+  string: string,
+  options?: QueryString.IParseOptions & { decoder?: undefined },
+) => parse(string, options);
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from './validator';
 
 export * from './convertNumberToUppercaseChinese';
 export * from './convertObjectToQueryString';
+export * from './convertQueryStringToObject';
 export * from './convertShortUuidV4ToUuidV4';
 export * from './convertUuidV4ToShortUuidV4';
 export * from './isEvalError';
@@ -15,6 +16,5 @@ export * from './isTypeError';
 export * from './isURIError';
 export * from './isValidDate';
 export * from './nanoid';
-export * from './parseQueryString';
 export * from './sleep';
 export * from './uuidV4';
