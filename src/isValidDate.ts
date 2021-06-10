b6d991e92aa19c98604062e99eda4f7f21cd6772
wurui <wurui7@mail3.sysu.edu.cn>
2021-06-10 14:35:50 +0800

    style: add period

--- src/isValidDate.ts
+++ src/isValidDate.ts
@@ -3,7 +3,7 @@ import { isDate, isNaN } from 'lodash-es';
 
 /**
  * Checks if value is classified as a valid Date object.
- * @param value - The value to check
+ * @param value - The value to check.
  * @return - Returns true if value is correctly classified, else false.
  */
 export const isValidDate = (value: any) =>
