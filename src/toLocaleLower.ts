941f7feaf812b82c0e78d83fe32849f6825f054f
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-06-19 22:27:26 +0800

    feat: remove toLocalLower and toLocalUpper

--- src/toLocaleLower.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Converts string, as a whole, to locale lower case.
- * @param string — The string to convert.
- * @return — Returns the locale lower cased string.
- */
-export const toLocaleLower = (string?: string) =>
-  (string ?? '').toString().toLocaleLowerCase();
--- src/toLocaleUpper.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Converts string, as a whole, to locale upper case.
- * @param string — The string to convert.
- * @return — Returns the locale upper cased string.
- */
-export const toLocaleUpper = (string?: string) =>
-  (string ?? '').toString().toLocaleUpperCase();
