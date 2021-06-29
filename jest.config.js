e8e960f44ca3a404f7a764c5e655fa05edfd57d9
wurui <wurui7@mail3.sysu.edu.cn>
2021-06-29 12:57:03 +0800

    build: remove jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest/presets/js-with-ts',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  testMatch: ['<rootDir>/tests/**/*.spec.ts'],
-  transformIgnorePatterns: ['node_modules/(?!(lodash-es)/)'],
-};
