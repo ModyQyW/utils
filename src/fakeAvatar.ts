86825976cf0f0d407a9547c5f85b6c796cfe8e85
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-07-28 10:39:42 +0800

    perf: avoid redirect

--- src/fakeAvatar.ts
+++ src/fakeAvatar.ts
@@ -1,4 +1,4 @@
-const domain = ['https://placekitten.com', 'https://unsplash.it'];
+const domain = ['https://placekitten.com', 'https://picsum.photos'];
 
 export const fakeAvatar = (size = 128) =>
   `${domain[Math.round(Math.random())]}/${size}`;
