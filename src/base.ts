028b633c0abc184a706487e6cad257cfceb1369a
ModyQyW <wurui-dev@foxmail.com>
2023-01-03 18:11:56 +0800

    feat: add getPackageJson

--- src/base.ts
+++ src/base.ts
@@ -1,9 +1,15 @@
+import { resolve } from 'node:path';
+import { readFileSync } from 'node:fs';
 import debounce from 'lodash.debounce';
 import throttle from 'lodash.throttle';
 import isEqual from 'lodash.isequal';
 import { deepmerge } from 'deepmerge-ts';
 import { nanoid } from 'nanoid';
+import { PackageJson } from './types';
 
 export { debounce, throttle, isEqual, deepmerge as merge, nanoid };
 
 export const timestamp = () => +Date.now();
+
+export const getPackageJson = () =>
+  JSON.parse(readFileSync(resolve(process.cwd(), 'package.json'), 'utf8')) as PackageJson;
