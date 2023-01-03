cedfe867b43e0e602cdcab6751d320fabe00a4f1
ModyQyW <wurui-dev@foxmail.com>
2023-01-03 17:21:16 +0800

    fix: fix rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,11 +1,11 @@
 import { readFileSync } from 'node:fs';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'rollup';
-import { rollupIndexConfig, rollupTypesConfig } from './src/rollup';
+import { rollupIndexConfig, rollupIndexTypesConfig } from './src/rollup';
 import type { PackageJson } from 'type-fest';
 
 const packageJson = JSON.parse(
   readFileSync(fileURLToPath(new URL('./package.json', import.meta.url)), 'utf-8'),
 ) as PackageJson;
 
-export default defineConfig([rollupIndexConfig(packageJson), rollupTypesConfig(packageJson)]);
+export default defineConfig([rollupIndexConfig(packageJson), rollupIndexTypesConfig(packageJson)]);
