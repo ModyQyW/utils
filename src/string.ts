1dea71cc4349bec8eeec63d242646fdcda5a08a0
ModyQyW <wurui-dev@foxmail.com>
2023-01-12 21:40:16 +0800

    fix: fix exports

--- src/string.ts
+++ src/string.ts
@@ -6,10 +6,14 @@ export {
   headerCase,
   noCase,
   paramCase,
+  paramCase as kebabCase,
   pascalCase,
   pathCase,
   sentenceCase,
   snakeCase,
 } from 'change-case';
+export * from 'lower-case';
+export * from 'upper-case';
+export * from 'title-case';
 
 export const toString = (v: any) => (v?.toString() ?? Object.prototype.toString.call(v)) as string;
