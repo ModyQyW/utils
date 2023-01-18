275823af798d376198bd0567bf43f8a9f53dacaa
ModyQyW <wurui-dev@foxmail.com>
2023-01-18 12:55:05 +0800

    feat!: remove `PQueue`

--- src/promise.ts
+++ src/promise.ts
@@ -1,11 +1,10 @@
 import pLimit from 'p-limit';
-import PQueue from 'p-queue';
 import pRetry from 'p-retry';
 import pDebounce from 'p-debounce';
 import pThrottle from 'p-throttle';
 import { Fn } from './types';
 
-export { pLimit, PQueue, pRetry, pDebounce, pThrottle };
+export { pLimit, pRetry, pDebounce, pThrottle };
 
 export function sleep(ms: number, callback?: Fn<any>) {
   return new Promise<void>((resolve) =>
