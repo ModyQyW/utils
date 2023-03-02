999fc0960cf3f5b5ee88cba3dd49f779f83255d4
ModyQyW <wurui-dev@foxmail.com>
2023-03-02 09:04:46 +0800

    feat: lowerCase, localeLowerCase, lowerCaseFirst, upperCase, localeUpperCase, upperCaseFirst

--- src/string.test.ts
+++ src/string.test.ts
@@ -1,7 +1,55 @@
 import { describe, it, expect } from 'vitest';
-import { toString } from './string';
+import {
+  lowerCase,
+  lowerCaseFirst,
+  localeLowerCase,
+  upperCase,
+  upperCaseFirst,
+  localeUpperCase,
+  toString,
+} from './string';
 
 describe('string', () => {
+  it('lowerCase', () => {
+    expect(lowerCase('')).toBe('');
+    expect(lowerCase('test')).toBe('test');
+    expect(lowerCase('TEST')).toBe('test');
+    expect(lowerCase('test string')).toBe('test string');
+    expect(lowerCase('TEST STRING')).toBe('test string');
+  });
+
+  it('localeLowerCase', () => {
+    expect(localeLowerCase('STRING', 'en-US')).toBe('string');
+  });
+
+  it('lowerCaseFirst', () => {
+    expect(lowerCaseFirst('')).toBe('');
+    expect(lowerCaseFirst('test')).toBe('test');
+    expect(lowerCaseFirst('TEST')).toBe('tEST');
+    expect(lowerCaseFirst('test string')).toBe('test string');
+    expect(lowerCaseFirst('TEST STRING')).toBe('tEST STRING');
+  });
+
+  it('upperCase', () => {
+    expect(upperCase('')).toBe('');
+    expect(upperCase('test')).toBe('TEST');
+    expect(upperCase('TEST')).toBe('TEST');
+    expect(upperCase('test string')).toBe('TEST STRING');
+    expect(upperCase('TEST STRING')).toBe('TEST STRING');
+  });
+
+  it('localeUpperCase', () => {
+    expect(localeUpperCase('string', 'en-US')).toBe('STRING');
+  });
+
+  it('upperCaseFirst', () => {
+    expect(upperCaseFirst('')).toBe('');
+    expect(upperCaseFirst('test')).toBe('Test');
+    expect(upperCaseFirst('TEST')).toBe('TEST');
+    expect(upperCaseFirst('test string')).toBe('Test string');
+    expect(upperCaseFirst('TEST STRING')).toBe('TEST STRING');
+  });
+
   it('toString', () => {
     expect(toString(null)).toBe('[object Null]');
     expect(toString(-0)).toBe('0');
--- src/string.ts
+++ src/string.ts
@@ -12,9 +12,27 @@ export {
   sentenceCase,
   snakeCase,
 } from 'change-case';
-export { lowerCase, localeLowerCase } from 'lower-case';
-export { upperCase, localeUpperCase } from 'upper-case';
 export { titleCase } from 'title-case';
+
+/** Convert string to lower case. */
+export const lowerCase = (string: string) => string.toLowerCase();
+
+/** Convert string to locale lower case. */
+export const localeLowerCase = (string: string, locales?: string | string[]) =>
+  string.toLocaleLowerCase(locales);
+
+/** Convert first char to lower case. */
+export const lowerCaseFirst = (string: string) => string.charAt(0).toLowerCase() + string.slice(1);
+
+/** Convert string to upper case. */
+export const upperCase = (string: string) => string.toUpperCase();
+
+/** Convert string to locale upper case. */
+export const localeUpperCase = (string: string, locales?: string | string[]) =>
+  string.toLocaleUpperCase(locales);
+
+/** Convert first char to upper case. */
+export const upperCaseFirst = (string: string) => string.charAt(0).toUpperCase() + string.slice(1);
 
 /**
  * Convert value to string.
