1e9eaae8beb9fc61670fdb5ef98e93539c965079
ModyQyW <wurui-dev@foxmail.com>
2023-03-02 15:28:59 +0800

    feat!: update types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,19 @@
+export type Primitive = null | undefined | string | number | boolean | symbol | bigint;
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type BuiltIns = Primitive | Date | RegExp | Function | Error;
+
+export type AnyRecord = Record<string, any>;
+
 export type Falsy = false | 0 | 0n | '' | null | undefined;
 
 export type Awaitable<T> = T | PromiseLike<T>;
 
 export type Nullable<T> = T | null;
+
+export type Undefinable<T> = T | undefined;
+
+export type Optional<T> = T | null | undefined;
 
 export type Arrayable<T> = T | Array<T>;
 
@@ -10,110 +21,4 @@ export type ElementOf<T> = T extends (infer E)[] ? E : never;
 
 export type Fn<T = void> = () => T;
 
-export type {
-  Primitive,
-  TypedArray,
-  Class,
-  Constructor,
-  JsonObject,
-  JsonArray,
-  JsonPrimitive,
-  JsonValue,
-  Unsubscribable,
-  Observer,
-  ObservableLike,
-  EmptyObject,
-  IsEmptyObject,
-  Except,
-  Writable,
-  Merge,
-  MergeDeep,
-  MergeDeepOptions,
-  MergeExclusive,
-  RequireAtLeastOne,
-  RequireExactlyOne,
-  RequireAllOrNone,
-  OmitIndexSignature,
-  PickIndexSignature,
-  PartialDeep,
-  PartialDeepOptions,
-  PartialOnUndefinedDeep,
-  PartialOnUndefinedDeepOptions,
-  ReadonlyDeep,
-  LiteralUnion,
-  Promisable,
-  Opaque,
-  UnwrapOpaque,
-  InvariantOf,
-  SetOptional,
-  SetRequired,
-  SetNonNullable,
-  ValueOf,
-  AsyncReturnType,
-  ConditionalExcept,
-  ConditionalKeys,
-  ConditionalPick,
-  ConditionalPickDeep,
-  ConditionalPickDeepOptions,
-  UnionToIntersection,
-  Stringified,
-  FixedLengthArray,
-  MultidimensionalArray,
-  MultidimensionalReadonlyArray,
-  IterableElement,
-  Entry,
-  Entries,
-  SetReturnType,
-  Asyncify,
-  Simplify,
-  Jsonify,
-  Jsonifiable,
-  Schema,
-  LiteralToPrimitive,
-  PositiveInfinity,
-  NegativeInfinity,
-  Finite,
-  Integer,
-  Float,
-  NegativeFloat,
-  Negative,
-  NonNegative,
-  NegativeInteger,
-  NonNegativeInteger,
-  StringKeyOf,
-  Exact,
-  ReadonlyTuple,
-  OptionalKeysOf,
-  HasOptionalKeys,
-  RequiredKeysOf,
-  Spread,
-  TupleToUnion,
-  IsEqual,
-  CamelCase,
-  CamelCasedProperties,
-  CamelCasedPropertiesDeep,
-  KebabCase,
-  KebabCasedProperties,
-  KebabCasedPropertiesDeep,
-  PascalCase,
-  PascalCasedProperties,
-  PascalCasedPropertiesDeep,
-  SnakeCase,
-  SnakeCasedProperties,
-  SnakeCasedPropertiesDeep,
-  ScreamingSnakeCase,
-  DelimiterCase,
-  DelimiterCasedProperties,
-  DelimiterCasedPropertiesDeep,
-  Join,
-  Split,
-  Trim,
-  Replace,
-  Includes,
-  Get,
-  LastArrayElement,
-  GlobalThis,
-  PackageJson,
-  TsConfigJson,
-  TsConfigJson as TsconfigJson,
-} from 'type-fest';
+export type { PackageJson, TsConfigJson } from 'type-fest';
