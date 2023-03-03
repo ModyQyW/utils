c1a9ea4a7464f0750b0b246df1d102fe9b083b46
ModyQyW <wurui-dev@foxmail.com>
2023-03-03 13:38:15 +0800

    feat!: self implement throttle and debounce

--- src/function.test.ts
+++ src/function.test.ts
@@ -1,7 +1,139 @@
-import { describe, it, expect } from 'vitest';
-import { pipe } from './function';
+import { describe, it, expect, beforeEach, vi, afterEach } from 'vitest';
+import { throttle, debounce, pipe, noop } from './function';
 
 describe('function', () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  const mockFn = vi.fn(noop);
+
+  it('throttle with { leading: true, trailing: true }', () => {
+    const throttled = throttle(mockFn, 1000);
+    throttled(); // called
+    throttled();
+    throttled();
+    throttled(); // called
+    setTimeout(throttled, 1200); // called
+    vi.advanceTimersByTime(2500);
+    expect(mockFn).toHaveBeenCalledTimes(3);
+  });
+
+  it('throttle with { leading: true, trailing: false }', () => {
+    const throttled = throttle(mockFn, 1000, { trailing: false });
+    throttled(); // called
+    throttled();
+    throttled();
+    throttled();
+    setTimeout(throttled, 1200); // called
+    vi.advanceTimersByTime(2500);
+    expect(mockFn).toHaveBeenCalledTimes(2);
+  });
+
+  it('throttle with { leading: false, trailing: true }', () => {
+    const throttled = throttle(mockFn, 1000, { leading: false });
+    throttled();
+    throttled();
+    throttled();
+    throttled(); // called
+    setTimeout(throttled, 1200); // called
+    vi.advanceTimersByTime(2500);
+    expect(mockFn).toHaveBeenCalledTimes(2);
+  });
+
+  it('throttle with { leading: false, trailing: false }', () => {
+    const throttled = throttle(mockFn, 1000, { leading: false, trailing: false });
+    throttled();
+    throttled();
+    throttled();
+    throttled();
+    setTimeout(throttled, 1200);
+    vi.advanceTimersByTime(2500);
+    expect(mockFn).toHaveBeenCalledTimes(0);
+  });
+
+  it('throttle then abort', () => {
+    const throttled = throttle(mockFn, 1000);
+    throttled(); // called
+    throttled();
+    throttled.abort(true);
+    throttled(); // called
+    vi.advanceTimersByTime(1200);
+    expect(throttled.isAborted()).toBe(false);
+    expect(mockFn).toHaveBeenCalledTimes(2);
+    throttled.abort();
+    throttled();
+    throttled();
+    vi.advanceTimersByTime(1200);
+    expect(throttled.isAborted()).toBe(true);
+    expect(mockFn).toHaveBeenCalledTimes(2);
+  });
+
+  it('debounce with { leading: false, trailing: true }', () => {
+    const debounced = debounce(mockFn, 1000);
+    debounced();
+    debounced();
+    debounced();
+    debounced(); // called
+    setTimeout(debounced, 1200); // called
+    vi.advanceTimersByTime(2500);
+    expect(mockFn).toHaveBeenCalledTimes(2);
+  });
+
+  it('debounce with {leading: false, trailing: false}', () => {
+    const debounced = debounce(mockFn, 1000, { leading: false, trailing: false });
+    debounced();
+    debounced();
+    debounced();
+    debounced();
+    setTimeout(debounced, 1200);
+    vi.advanceTimersByTime(2500);
+    expect(mockFn).toHaveBeenCalledTimes(0);
+  });
+
+  it('debounce with {leading: true, trailing: true}', () => {
+    const debounced = debounce(mockFn, 1000, { leading: true, trailing: true });
+    debounced(); // called
+    debounced();
+    debounced();
+    debounced(); // called
+    setTimeout(debounced, 1200); // called
+    vi.advanceTimersByTime(2500);
+    expect(mockFn).toHaveBeenCalledTimes(3);
+  });
+
+  it('debounce with {leading: true, trailing: false}', () => {
+    const debounced = debounce(mockFn, 1000, { leading: true, trailing: false });
+    debounced(); // called
+    debounced();
+    debounced();
+    debounced();
+    setTimeout(debounced, 1200); // called
+    vi.advanceTimersByTime(2500);
+    expect(mockFn).toHaveBeenCalledTimes(2);
+  });
+
+  it('debounce then abort', () => {
+    const debounced = debounce(mockFn, 1000);
+    debounced();
+    debounced();
+    debounced.abort(true);
+    debounced(); // called
+    vi.advanceTimersByTime(1200);
+    expect(debounced.isAborted()).toBe(false);
+    expect(mockFn).toHaveBeenCalledTimes(1);
+    debounced.abort();
+    debounced();
+    debounced();
+    vi.advanceTimersByTime(1200);
+    expect(debounced.isAborted()).toBe(true);
+    expect(mockFn).toHaveBeenCalledTimes(1);
+  });
+
   it('pipe', () => {
     expect(pipe((a: number, b: number) => a + b)(1, 1)).toBe(2);
     expect(
--- src/function.ts
+++ src/function.ts
@@ -1,8 +1,90 @@
-// export { throttle, debounce } from 'throttle-debounce'
-
 /** Empty function. */
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export function noop() {}
+
+function throttleOrDebounce<Callback extends (...args: any) => any>(
+  callback: Callback,
+  delay = 0,
+  {
+    leading = true,
+    trailing = true,
+    mode = 'throttle',
+  }: { leading?: boolean; trailing?: boolean; mode?: 'throttle' | 'debounce' } = {},
+) {
+  let timerId: ReturnType<typeof setTimeout>;
+  function clearTimer() {
+    if (timerId) clearTimeout(timerId);
+  }
+  let isAborted = false;
+  function abort(isOneTime = false) {
+    clearTimer();
+    isAborted = !isOneTime;
+  }
+  let lastRun = 0;
+  function wrapper(...args: Parameters<Callback>) {
+    function run() {
+      lastRun = Date.now();
+      // @ts-expect-error no types
+      callback.apply(this, args);
+    }
+    if (isAborted) return;
+    const sinceLastRun = Date.now() - lastRun;
+    clearTimer();
+    if (sinceLastRun >= delay) {
+      if (leading) {
+        run();
+      } else {
+        lastRun = Date.now();
+        if (trailing) timerId = setTimeout(run, delay);
+      }
+    } else if (trailing) {
+      timerId = setTimeout(run, mode === 'throttle' ? delay - sinceLastRun : delay);
+    }
+  }
+  wrapper.abort = abort;
+  wrapper.cancel = abort;
+  wrapper.isAborted = function () {
+    return isAborted;
+  };
+  wrapper.isCancelled = wrapper.isAborted;
+  return wrapper;
+}
+
+/**
+ * Creates a throttled function that only invokes `callback` at most once per every `delay`
+ * milliseconds.
+ *
+ * @param callback function to throttle
+ * @param delay milliseconds to throttle
+ * @param options options object, default `{}`
+ * @param options.leading specify invoking on the leading edge of the timeout, default `true`
+ * @param options.trailing specify invoking on the trailing edge of the timeout, default `true`
+ */
+export function throttle<Callback extends (...args: any) => any>(
+  callback: Callback,
+  delay = 0,
+  { leading = true, trailing = true } = {},
+) {
+  return throttleOrDebounce(callback, delay, { leading, trailing, mode: 'throttle' });
+}
+
+/**
+ * Creates a debounced function that delays invoking `callback` until after `delay` milliseconds
+ * have elapsed since the last time the debounced function was invoked.
+ *
+ * @param callback function to throttle
+ * @param delay milliseconds to debounce
+ * @param options options object, default `{}`
+ * @param options.leading specify invoking on the leading edge of the timeout, default `false`
+ * @param options.trailing specify invoking on the trailing edge of the timeout, default `true`
+ */
+export function debounce<Callback extends (...args: any) => any>(
+  callback: Callback,
+  delay = 0,
+  { leading = false, trailing = true } = {},
+) {
+  return throttleOrDebounce(callback, delay, { leading, trailing, mode: 'debounce' });
+}
 
 export function pipe<Args extends any[], Result1>(
   fn1: (...args: Args) => Result1,
