4a6a6c47d47c55a91cfba8cafef3da495fb05eb4
ModyQyW <wurui-dev@foxmail.com>
2023-08-08 18:20:18 +0800

    feat!: update types

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,36 @@
-import { Promisable } from 'type-fest';
-
 export type AnyRecord = Record<string, any>;
 
 export type Falsy = false | 0 | 0n | '' | null | undefined;
 
-export type Awaitable<T> = Promisable<T>;
+export type MaybePromise<T> = T | Promise<T>;
+
+export type MaybePromiseLike<T> = T | PromiseLike<T>;
+
+export type Promisable<T> = T | PromiseLike<T>;
+
+export type Awaitable<T> = T | PromiseLike<T>;
+
+export type MaybeNull<T> = T | null;
 
 export type Nullable<T> = T | null;
 
+export type NonNullable<T> = Exclude<T, null>;
+
+export type MaybeUndefined<T> = T | undefined;
+
+export type Optional<T> = T | undefined;
+
 export type Undefinable<T> = T | undefined;
 
-export type Optional<T> = T | null | undefined;
+export type NonUndefinable<T> = Exclude<T, undefined>;
 
-export type Arrayable<T> = T | Array<T>;
+export type Nullish<T> = T | undefined | null;
+
+export type NonNullish<T> = Exclude<T, undefined | null>;
+
+export type MaybeArray<T> = T | T[];
+
+export type Arrayable<T> = T | T[];
 
 export type ElementOf<T> = T extends (infer E)[] ? E : never;
 
@@ -23,5 +41,3 @@ export type PromisifyFn<T extends Fn> = () => Promise<ReturnType<T>>;
 export type AnyFn<T = any> = (...args: any[]) => T;
 
 export type PromisifyAnyFn<T extends AnyFn> = (...args: Parameters<T>) => Promise<ReturnType<T>>;
-
-export type * from 'type-fest';
